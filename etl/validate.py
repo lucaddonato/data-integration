783ceaca57c52dc92769c501328eb465e1a28017
Luca <123180562+lucaddonato@users.noreply.github.com>
2026-05-29 20:46:08 -0300

    add data-2

--- etl/validate.py
+++ etl/validate.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 conn = psycopg2.connect(
     host="postgres",
     database="movies_db",
@@ -9,13 +10,44 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute("SELECT COUNT(*) FROM movies;")
+
+cur.execute("SELECT COUNT(*) FROM fact_movie_metrics;")
 total = cur.fetchone()[0]
 
 if total == 0:
-    raise ValueError("A tabela movies está vazia")
+    raise ValueError("Validação falhou: a tabela fato está vazia")
 
-print(f"Validação concluída. Total de filmes na tabela: {total}")
+
+cur.execute("""
+SELECT COUNT(*)
+FROM fact_movie_metrics
+WHERE budget < 0
+   OR revenue < 0
+   OR profit < 0
+   OR kaggle_runtime < 0;
+""")
+invalid_ranges = cur.fetchone()[0]
+
+if invalid_ranges > 0:
+    raise ValueError("Validação falhou: existem valores negativos nas métricas")
+
+
+cur.execute("""
+SELECT COUNT(*)
+FROM fact_movie_metrics f
+LEFT JOIN dim_movie d
+    ON f.movie_id = d.movie_id
+WHERE d.movie_id IS NULL;
+""")
+missing_movies = cur.fetchone()[0]
+
+if missing_movies > 0:
+    raise ValueError("Validação falhou: existem filmes na fato sem dimensão correspondente")
+
+
+print("Validações concluídas com sucesso")
+print(f"Total de registros validados: {total}")
+
 
 cur.close()
 conn.close()

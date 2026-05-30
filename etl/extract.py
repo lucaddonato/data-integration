ae417e3f689dfb601d0b6613daa7863b17b96636
Luca <123180562+lucaddonato@users.noreply.github.com>
2026-05-29 21:19:31 -0300

    20 filmes

--- etl/extract.py
+++ etl/extract.py
@@ -4,13 +4,72 @@ import requests
 
 API_KEY = os.getenv("TMDB_API_KEY")
 
-url = f"https://api.themoviedb.org/3/movie/550?api_key={API_KEY}"
+movies_names = [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "The Godfather Part II",
+    "The Dark Knight",
+    "Pulp Fiction",
+    "Fight Club",
+    "Forrest Gump",
+    "Schindler's List",
+    "Goodfellas",
+    "The Green Mile",
+    "The Lord of the Rings: The Return of the King",
+    "Inception",
+    "Interstellar",
+    "Parasite",
+    "Whiplash",
+    "The Prestige",
+    "Se7en",
+    "The Silence of the Lambs",
+    "Saving Private Ryan",
+    "Gladiator"
+]
 
-response = requests.get(url)
+movies = []
 
-movie = response.json()
+for movie_name in movies_names:
 
-with open("/opt/airflow/data/movies.json", "w", encoding="utf-8") as f:
-    json.dump(movie, f, ensure_ascii=False, indent=4)
+    url = (
+        f"https://api.themoviedb.org/3/search/movie"
+        f"?api_key={API_KEY}"
+        f"&query={movie_name}"
+    )
 
-print("Arquivo salvo com sucesso")
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Erro ao buscar {movie_name}")
+        continue
+
+    data = response.json()
+
+    if not data.get("results"):
+        print(f"Filme não encontrado: {movie_name}")
+        continue
+
+    movie_id = data["results"][0]["id"]
+
+    details_url = (
+        f"https://api.themoviedb.org/3/movie/{movie_id}"
+        f"?api_key={API_KEY}"
+    )
+
+    details = requests.get(details_url).json()
+
+    movies.append(details)
+
+with open(
+    "/opt/airflow/data/movies.json",
+    "w",
+    encoding="utf-8"
+) as f:
+    json.dump(
+        movies,
+        f,
+        ensure_ascii=False,
+        indent=4
+    )
+
+print(f"{len(movies)} filmes extraídos com sucesso")
--- etl/transform.py
+++ etl/transform.py
@@ -2,27 +2,47 @@ import json
 import pandas as pd
 
 with open("/opt/airflow/data/movies.json", "r", encoding="utf-8") as f:
-    movie = json.load(f)
+    movies = json.load(f)
 
 csv_df = pd.read_csv("/opt/airflow/data/kaggle_movies.csv")
 
-csv_movie = csv_df[csv_df["id"] == movie["id"]].iloc[0]
+transformed_movies = []
 
-transformed_movie = {
-    "id": movie["id"],
-    "title": movie["title"],
-    "release_date": movie["release_date"],
-    "budget": movie["budget"],
-    "revenue": movie["revenue"],
-    "vote_average": movie["vote_average"],
-    "original_language": movie["original_language"],
-    "status": movie["status"],
-    "kaggle_score": float(csv_movie["kaggle_score"]),
-    "kaggle_runtime": int(csv_movie["kaggle_runtime"])
-}
+for movie in movies:
 
-with open("/opt/airflow/data/transformed_movies.json", "w", encoding="utf-8") as f:
-    json.dump(transformed_movie, f, ensure_ascii=False, indent=4)
+    csv_match = csv_df[csv_df["id"] == movie["id"]]
 
-print("Transformação concluída")
-print(transformed_movie)
+    if csv_match.empty:
+        print(f"Filme não encontrado no CSV: {movie['title']}")
+        continue
+
+    csv_movie = csv_match.iloc[0]
+
+    transformed_movie = {
+        "id": movie["id"],
+        "title": movie["title"],
+        "release_date": movie["release_date"],
+        "budget": movie["budget"],
+        "revenue": movie["revenue"],
+        "vote_average": movie["vote_average"],
+        "original_language": movie["original_language"],
+        "status": movie["status"],
+        "kaggle_score": float(csv_movie["kaggle_score"]),
+        "kaggle_runtime": int(csv_movie["kaggle_runtime"])
+    }
+
+    transformed_movies.append(transformed_movie)
+
+with open(
+    "/opt/airflow/data/transformed_movies.json",
+    "w",
+    encoding="utf-8"
+) as f:
+    json.dump(
+        transformed_movies,
+        f,
+        ensure_ascii=False,
+        indent=4
+    )
+
+print(f"{len(transformed_movies)} filmes transformados com sucesso")

c6c857ffbabfc4a009c68192b19dcde7bf736256
Vinicius Silva <72896107+Vinicius-eng20@users.noreply.github.com>
2026-06-05 19:46:28 -0300

    fix: extract and postgres

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,35 +1,29 @@
 -- Consulta 1: Top 10 filmes por lucro
 
 SELECT
-    m.title,
-    f.profit
-FROM fact_movie_metrics f
-JOIN dim_movie m
-    ON f.movie_id = m.movie_id
-ORDER BY f.profit DESC
+    title,
+    (revenue - budget) AS profit
+FROM fact_movies
+ORDER BY profit DESC
 LIMIT 10;
 
 
 -- Consulta 2: Top 10 filmes mais bem avaliados
 
 SELECT
-    m.title,
-    f.kaggle_score,
-    f.vote_average
-FROM fact_movie_metrics f
-JOIN dim_movie m
-    ON f.movie_id = m.movie_id
-ORDER BY f.kaggle_score DESC
+    title,
+    vote_average,
+    vote_count
+FROM fact_movies
+ORDER BY vote_average DESC
 LIMIT 10;
 
 
 -- Consulta 3: Top 10 filmes por receita
 
 SELECT
-    m.title,
-    f.revenue
-FROM fact_movie_metrics f
-JOIN dim_movie m
-    ON f.movie_id = m.movie_id
-ORDER BY f.revenue DESC
+    title,
+    revenue
+FROM fact_movies
+ORDER BY revenue DESC
 LIMIT 10;
